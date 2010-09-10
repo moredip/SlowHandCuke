5222074107ef1edf5f346ad0913514ff94cfa431
Pete Hodgson <git@thepete.net>
2010-09-10 10:30:15 -0700

    Replace fancy-pants ansi codes with simple carriage-return (doh!)

--- formatter/slow_hand_cuke.rb
+++ formatter/slow_hand_cuke.rb
@@ -2,21 +2,13 @@ require 'cucumber/formatter/pretty'
 module Formatter
   class SlowHandCuke < Cucumber::Formatter::Pretty
     def before_step( step )
-      print_ansi_code 's'  # save cursor position 
       @io.printf "... #{step.name}"
       @io.flush
     end
 
     def before_step_result( *args )
-      print_ansi_code '1K' # clear current line
-      print_ansi_code 'u'  # move cursor back to start of line
+      @io.printf "\r"
       super
-    end
-
-    private
-
-    def print_ansi_code(code)
-      @io.printf "\033[#{code}"
     end
   end
 end
